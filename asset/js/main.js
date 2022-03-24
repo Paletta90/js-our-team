0d65eadb359966263979892b53a2ed6f70176fea
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-24 15:30:25 +0100

    Bonus

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,10 @@
 // Variabile del div container
 let container = document.getElementById("container");
+//Variabile per le card
+let containerCard = document.getElementById("containerCard");
 
 // Array di oggetti contenete i vari membri del team
-let listMembers = [
-    {
+let listMembers = [{
         'Nome': 'Wayne Barrett',
         'Ruolo': 'Founders & CO',
         'Immagine': 'new-team-member-04.jpg'
@@ -42,9 +43,9 @@ let listMembers = [
 ];
 
 // Stampo tutti i rispettivi dati dei vari oggetti contenuti nell'array di oggetti
-for(let i = 0; i < listMembers.length; i++){
+for (let i = 0; i < listMembers.length; i++) {
 
-    for(let key in listMembers[i]){
+    for (let key in listMembers[i]) {
 
         console.log(`${key}: ${listMembers[i][key]}`)
 
@@ -53,12 +54,12 @@ for(let i = 0; i < listMembers.length; i++){
 }
 
 // Stampo i vari dati dei mebri a video
-for(let i = 0; i < listMembers.length; i++){
+for (let i = 0; i < listMembers.length; i++) {
 
     // Creo un div per ogni membro del team
     let containerMembers = document.createElement('div');
     containerMembers.innerHTML += `<h4>Membro numero ${i}:</h4>`
-    for(let key in listMembers[i]){
+    for (let key in listMembers[i]) {
 
         // In ogni div stampo i dati del mebro del team
         containerMembers.innerHTML += `<p>- ${key}: ${listMembers[i][key]}</p>`
@@ -70,3 +71,19 @@ for(let i = 0; i < listMembers.length; i++){
 
 }
 
+// Stampo a video le relative card
+for (let i = 0; i < listMembers.length; i++) {
+
+    containerCard.innerHTML += `
+        
+            <div class="col">
+                <div class="card h-100 text-center">
+                    <img src="./asset/img/${listMembers[i].Immagine}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">${listMembers[i].Nome}</h4>
+                        <p class="card-text">${listMembers[i].Ruolo}</p>
+                    </div>
+                </div>
+            </div>`
+
+}
